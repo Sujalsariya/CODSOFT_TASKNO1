39da0b203f95f413a9fcc185a07cc2bad5d19c0d
Sujalsariya <119974733+Sujalsariya@users.noreply.github.com>
2023-08-14 22:41:36 +0530

    Add files via upload

---
+++ quiz_data.py
@@ -0,0 +1,29 @@
+quiz_data = [
+    {
+        "question": "How many planets in our Solar System ?",
+        "choices": ["A.4","B.5","C.10","D.8"],
+        "answer": "D.8"
+    },
+    {
+        "question": "How many district in Rajasthan ?",
+        "choices": ["A.33","B.56","C.35","D.50"],
+        "answer": "D.50"
+    },
+    {
+        "question":"How many states in India?" ,
+        "choices": ["A.28","B.30","C.29","D.25"],
+        "answer": "A.28"
+    },
+    {
+        "question":"How many bones are in human body ?" ,
+        "choices": ["A.208","B.210","C.206","D.205"],
+        "answer": "C.206"
+    },
+    {
+         "question":"How many binary numbers?",
+        "choices": ["A.2","B.1","C.3","D.5"],
+        "answer": "A.2"
+    }
+    
+    # Add more questions here
+]
